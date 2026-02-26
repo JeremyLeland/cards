19650441ddab55619268b9ba403d27291cf33104
Jeremy Leland <jeremy.leland@gmail.com>
2026-02-25 19:40:18 -0800

    Try to make rasterized SVG look sharp, still feels blurry to me

--- test/createImageBitmapFromSVG.js
+++ test/createImageBitmapFromSVG.js
@@ -1,42 +1,56 @@
+// This still seems blurry, possibly due to innacurate CARD_WIDTH or inexactness of rasterizing SVG
+
 const CARD_WIDTH = 0.64;
 const CARD_HEIGHT = 1;
 
 const SHEET_COLS = 13;
 const SHEET_ROWS = 5;
 
+const cssWidth = 900;
+const cssHeight = 600;
 
-import { Canvas } from '../src/common/Canvas.js';
+// HiDPI canvas needs larger image for same display size
+const canvas = document.createElement( 'canvas' );
+canvas.style.width = cssWidth + 'px';
+canvas.style.height = cssHeight + 'px';
+canvas.width = cssWidth * devicePixelRatio;
+canvas.height = cssHeight * devicePixelRatio;                             
 
-const canvas = new Canvas();
-canvas.backgroundColor = '#123';
-canvas.bounds = [ 0, 0, 1, 1 ];
+document.body.appendChild( canvas );
 
+const ctx = canvas.getContext( '2d' );
+ctx.scale( devicePixelRatio, devicePixelRatio );
 
-// const canvas = document.createElement( 'canvas' );
-// canvas.width = 800;
-// canvas.height = 600;
-// document.body.appendChild( canvas );
-
-const ctx = canvas.ctx;//getContext( '2d' );
 const image = new Image();
-
 image.onload = async () => {
 
-  const size = 200;
+  const size = 300;
 
+  // Need to create HiDPI image to draw from
   const bitmap = await createImageBitmap( 
     image, 
     { 
-      resizeWidth:  size * SHEET_COLS * CARD_WIDTH,
-      resizeHeight: size * SHEET_ROWS * CARD_HEIGHT, 
+      resizeWidth:  size * SHEET_COLS * CARD_WIDTH * devicePixelRatio,
+      resizeHeight: size * SHEET_ROWS * CARD_HEIGHT * devicePixelRatio,
       resizeQuality: 'high' 
     } 
   );
 
-  for ( let i = 0; i < 10; i ++ ) {
-    ctx.drawImage( bitmap, 
-      i * size * CARD_WIDTH, 0, size * CARD_WIDTH, size * CARD_HEIGHT, 
-      i * size * CARD_WIDTH / 3, i * size * CARD_HEIGHT / 4, size * CARD_WIDTH, size * CARD_HEIGHT,
+  for ( let i = 0; i < 13; i ++ ) {
+    ctx.drawImage( 
+      bitmap, 
+
+      // source image is HiDPI
+      i * size * CARD_WIDTH * devicePixelRatio, 
+      0, 
+      size * CARD_WIDTH * devicePixelRatio,   
+      size * CARD_HEIGHT * devicePixelRatio,
+      
+      // destination is css location
+      i * size * CARD_WIDTH, 
+      ( i % 2 ) * size * CARD_HEIGHT, 
+      size * CARD_WIDTH, 
+      size * CARD_HEIGHT,
     );
   }
 }
