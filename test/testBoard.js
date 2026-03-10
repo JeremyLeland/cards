86fa69b2dddfccc841a4ec2feaac5c606935eaff
Jeremy Leland <jeremy.leland@gmail.com>
2026-03-09 21:40:33 -0700

    Use SVGCard for testBoard

--- test/testBoard.js
+++ test/testBoard.js
@@ -1,22 +1,7 @@
-// import * as Card from '../src/Card.js'
+import * as Card from '../src/SVGCard.js'
 
 import { GameCanvas } from '../src/common/GameCanvas.js';
 import { GameState } from '../src/common/GameState.js';
-
-const Card = {
-  NumRanks: 13,
-  NumSuits: 4,
-  Width: 120,
-};
-
-const image = new Image();
-image.src = './images/Aisleriot_-_Anglo_playing_cards.svg';
-await image.decode();
-
-const imageCardWidth = image.width / Card.NumRanks;
-const imageCardHeight = image.height / ( Card.NumSuits + 1 );   // additional line has jokers and card back
-
-Card.Height = Card.Width * imageCardHeight / imageCardWidth;
 
 
 const Gap = 20;
@@ -87,19 +72,6 @@ function shuffle( array ) {
 }
 
 
-// Better or worse than createImageBitmap? Supposedly that goes to GPU...
-// TODO: Maybe make an ImageBitmap from the offscreen canvas?
-// Seemed like createImageBitmap scaling looked crappy before, but I might've been doing something else wrong
-
-// Rasterize a larger version of SVG to use
-const scale = devicePixelRatio;// * Card.Width / imageCardWidth;
-const off = document.createElement( 'canvas' );
-off.width = image.width * scale;
-off.height = image.height * scale;
-
-const offCtx = off.getContext( '2d' );
-offCtx.scale( scale, scale );
-offCtx.drawImage( image, 0, 0 );
 
 // Prepare canvas
 const gameCanvas = new GameCanvas( HorizSpacing * 7, Math.round( VertSpacing * 4 ) );
@@ -108,8 +80,10 @@ gameCanvas.draw = ( ctx ) => {
   ctx.fillStyle = '#123';
   ctx.fillRect( 0, 0, ctx.canvas.width, ctx.canvas.height );
 
+  ctx.translate( 0.5 * Card.Width, 0.5 * Card.Height );
+
   if ( gameState.board.stock.length > 0 ) {
-    drawCard( ctx, gameState.board.stock.at( -1 ), 0, 0 );
+    Card.draw( ctx, gameState.board.stock.at( -1 ), 0, 0 );
   }
   else {
     drawCardOutline( ctx, 0, 0 );
@@ -129,7 +103,7 @@ gameCanvas.draw = ( ctx ) => {
         break;
       }
 
-      drawCard(
+      Card.draw(
         ctx,
         gameState.board.waste[ wasteCardIndex ],
         HorizSpacing + WasteOffset.x * cIndex,
@@ -146,7 +120,7 @@ gameCanvas.draw = ( ctx ) => {
       const topCard = foundation == active?.oldStack ? foundation.at( -2 ) : foundation.at( -1 );
 
       if ( topCard ) {
-        drawCard( 
+        Card.draw( 
           ctx, 
           topCard, 
           Positions.Foundations[ fIndex ].x, 
@@ -162,7 +136,7 @@ gameCanvas.draw = ( ctx ) => {
         break;
       }
 
-      drawCard(
+      Card.draw(
         ctx,
         tableau[ cIndex ],
         Positions.Tableaus[ tIndex ].x,
@@ -173,7 +147,7 @@ gameCanvas.draw = ( ctx ) => {
 
   if ( active ) {
     for ( let cIndex = active.oldStartIndex; cIndex < active.oldStack.length; cIndex ++ ) {
-      drawCard(
+      Card.draw(
         ctx,
         active.oldStack[ cIndex ],
         active.pos.x,
@@ -185,49 +159,12 @@ gameCanvas.draw = ( ctx ) => {
 
 function drawCardOutline( ctx, x, y ) {
   ctx.beginPath();
-  ctx.roundRect( x, y, Card.Width, Card.Height, 5 );
+  ctx.roundRect( x - Card.Width / 2, y - Card.Height / 2, Card.Width, Card.Height, 5 );
   ctx.strokeStyle = 'white';
   ctx.stroke();
 }
 
 gameCanvas.redraw();
-
-// If card is null, draw back
-function drawCard( ctx, card, x, y ) {
-
-  const srcCol = card.faceup ? card.rank : 2;
-  const srcRow = card.faceup ? card.suit : 4;
-
-  ctx.drawImage(
-    off,
-
-    // source image is HiDPI
-    srcCol * devicePixelRatio * imageCardWidth, //Card.Width * devicePixelRatio,
-    srcRow * devicePixelRatio * imageCardHeight, //Card.Height * devicePixelRatio,
-    devicePixelRatio * imageCardWidth, //Card.Width * devicePixelRatio,
-    devicePixelRatio * imageCardHeight, //Card.Height * devicePixelRatio,
-
-    // destination is screen location
-    x,
-    y,
-    Card.Width,
-    Card.Height,
-  );
-
-  // This is much less blurry. How can I fix my pre-render?
-  // ctx.drawImage(
-  //   image,
-  //   srcCol * imageCardWidth,
-  //   srcRow * imageCardHeight,
-  //   imageCardWidth,
-  //   imageCardHeight,
-
-  //   x,
-  //   y,
-  //   Card.Width,
-  //   Card.Height,
-  // );
-}
 
 
 //
